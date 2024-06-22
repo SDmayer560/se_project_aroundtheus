86c55f6fe3d88aedf16a52a995c7515144b0ae4c
McKinzie <mckinzie.logan@gmail.com>
2024-06-22 03:52:19 -0700

    adding Javascript file and beginnings of committed

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+let object1 {
+    name: "Yosemite Valley",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg"
+}
+
+let object2 {
+    name: "Lake Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg"
+}
+
+ let object3 {
+    name: "Bald Mountains",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg"
+ }
+
+ let object4 {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg"
+ }
+
+ let object5 {
+    name: "Vanoise National Park",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg"
+ }
+
+ let object6 {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg"
+ }
+
+let initialCards = [object1, object2, object3, object4, object5, object6];
